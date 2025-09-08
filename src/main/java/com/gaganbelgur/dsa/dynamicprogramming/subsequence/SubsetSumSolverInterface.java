3afdb7eeab90cbb2eac244f1c097c31a0254e14d
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-08 23:28:45 +0530

    SubSetSumSolver Problem  Solution

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/subsequence/SubsetSumSolverInterface.java
@@ -0,0 +1,6 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.subsequence;
+
+public interface SubsetSumSolverInterface {
+
+    boolean isSubsetSumPresent(int[] arr, int target);
+}
---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/subsequence/SubsetSumSolverRecursion.java
@@ -0,0 +1,23 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.subsequence;
+
+public class SubsetSumSolverRecursion implements SubsetSumSolverInterface {
+
+    @Override
+    public boolean isSubsetSumPresent(int[] arr, int target) {
+        return isSubsetPresent(arr, arr.length - 1, target);
+    }
+
+    private boolean isSubsetPresent(int[] arr, int index, int target) {
+        if (target == 0) return true;
+        if (index == 0) return arr[index] == target;
+
+        boolean notTake = isSubsetPresent(arr, index - 1, target);
+        boolean take = false;
+
+        if (target >= arr[index]) {
+            take = isSubsetPresent(arr, index - 1, target - arr[index]);
+        }
+
+        return take || notTake;
+    }
+}
