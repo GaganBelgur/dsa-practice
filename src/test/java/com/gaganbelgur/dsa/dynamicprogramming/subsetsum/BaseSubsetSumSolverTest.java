69a6c07f9e1a6a65f6c7ae9738e74a211e0afdc6
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-10 23:58:41 +0530

    Added unit test case SubSetSumSolver Problem  Solution by Tabulation.

---
+++ src/test/java/com/gaganbelgur/dsa/dynamicprogramming/subsetsum/BaseSubsetSumSolverTest.java
@@ -0,0 +1,27 @@
+package test.java.com.gaganbelgur.dsa.dynamicprogramming.subsetsum;
+
+import main.java.com.gaganbelgur.dsa.dynamicprogramming.subsequence.SubsetSumSolverInterface;
+import main.java.com.gaganbelgur.dsa.dynamicprogramming.threedimension.AliceBobChocolatesSolver;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public abstract class BaseSubsetSumSolverTest {
+
+    protected abstract SubsetSumSolverInterface getSolver();
+
+    @Test
+    public void GivenValidArrayWhenCheckIsSubsetHasPresentMethodIsInvokedThenTrueValueIsReturned() {
+        int[] array = {2, 3, 1, 2};
+
+        assertTrue(getSolver().isSubsetSumPresent(array, 5));
+    }
+
+    @Test
+    public void GivenValidArrayAndTarget0WhenCheckIsSubsetHasPresentMethodIsInvokedThenTrueValueIsReturned() {
+        int[] array = {2, 3, 1, 2};
+
+        assertTrue(getSolver().isSubsetSumPresent(array, 0));
+    }
+}
