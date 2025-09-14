d15a6ec3cf05e617a77680d3d79a18234e625a3e
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-14 23:11:17 +0530

    Added ThreeSumSolverBruteForce.

---
+++ src/main/java/com/gaganbelgur/dsa/arrays/ThreeSumSolverBruteForce.java
@@ -0,0 +1,31 @@
+package main.java.com.gaganbelgur.dsa.arrays;
+
+import java.util.*;
+
+public class ThreeSumSolverBruteForce implements ThreeSumSolverInterface {
+
+    @Override
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        return threeSum(nums, n);
+    }
+
+    private List<List<Integer>> threeSum(int[] nums, int n) {
+        Set<List<Integer>> result = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        result.add(list);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
---
+++ src/main/java/com/gaganbelgur/dsa/arrays/ThreeSumSolverInterface.java
@@ -0,0 +1,8 @@
+package main.java.com.gaganbelgur.dsa.arrays;
+
+import java.util.List;
+
+public interface ThreeSumSolverInterface {
+
+    List<List<Integer>> threeSum(int[] nums);
+}
