f8a15d1a26a842a72be1524e55cea80e73e2086e
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-17 22:52:14 +0530

    Refactored SearchTargetInTwoDimensionMatrixSolverInterface

--- src/main/java/com/gaganbelgur/dsa/binarysearch/searchelement/SearchTargetInTwoDimensionMatrixSolverInterface.java
+++ src/main/java/com/gaganbelgur/dsa/binarysearch/searchelement/SearchTargetInTwoDimensionMatrixSolverInterface.java
@@ -1,4 +1,4 @@
-package main.java.com.gaganbelgur.dsa.binarysearch;
+package main.java.com.gaganbelgur.dsa.binarysearch.searchelement;
 
 public interface SearchTargetInTwoDimensionMatrixSolverInterface {
 
