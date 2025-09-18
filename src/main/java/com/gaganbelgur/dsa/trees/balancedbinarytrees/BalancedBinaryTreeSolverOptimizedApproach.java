a0c8a72eff7f9068724b2a2f2fc684c7d11283a4
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-18 23:31:48 +0530

    Added BalancedBinaryTreeSolverInterface

--- src/main/java/com/gaganbelgur/dsa/trees/balancedbinarytrees/BalancedBinaryTreeSolverOptimizedApproach.java
+++ src/main/java/com/gaganbelgur/dsa/trees/balancedbinarytrees/BalancedBinaryTreeSolverOptimizedApproach.java
@@ -4,6 +4,20 @@ public class BalancedBinaryTreeSolverOptimizedApproach implements BalancedBinary
 
     @Override
     public boolean isBalanced(TreeNode root) {
-        return false;
+        return dfsHeight(root) != -1;
+    }
+
+    private int dfsHeight(TreeNode node) {
+        if(node == null) return 0;
+
+        int leftHeight = dfsHeight(node.left);
+        if(leftHeight == -1) return -1;
+
+        int rightHeight = dfsHeight(node.right);
+        if(rightHeight == -1) return -1;
+
+        if(Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
