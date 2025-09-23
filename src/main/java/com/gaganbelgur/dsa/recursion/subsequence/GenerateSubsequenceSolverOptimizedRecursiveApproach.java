d11f98e3c65b34f35863b941c900bae7b66a3be0
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-23 22:59:14 +0530

    Added GenerateSubsequenceSolverOptimizedRecursiveApproach

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/subsequence/GenerateSubsequenceSolverOptimizedRecursiveApproach.java
@@ -0,0 +1,23 @@
+package main.java.com.gaganbelgur.dsa.recursion.subsequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateSubsequenceSolverOptimizedRecursiveApproach implements GenerateSubsequenceSolverInterface {
+
+    @Override
+    public List<List<Integer>> powerSet(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+    private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+         result.add(new ArrayList<>(current));
+
+         for (int i = index; i < nums.length; i++) {
+           current.add(nums[i]);
+           generateSubsets(nums, i + 1, current, result);
+           current.remove(current.size() - 1);
+         }
+    }
+}
