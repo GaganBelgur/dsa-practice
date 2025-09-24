38e7ca6bbc0e8bffcd21042746bea70b317385f6
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-24 23:56:42 +0530

    Added Operations

---
+++ src/main/java/com/gaganbelgur/dsa/linkedlist/InsertOperations.java
@@ -0,0 +1,7 @@
+package main.java.com.gaganbelgur.dsa.linkedlist;
+
+public interface InsertOperations extends Operations {
+
+    ListNode insertAtHead(ListNode head, int data);
+
+}
---
+++ src/main/java/com/gaganbelgur/dsa/linkedlist/Operations.java
@@ -0,0 +1,4 @@
+package main.java.com.gaganbelgur.dsa.linkedlist;
+
+public interface Operations {
+}
