18579ba2c69e9bb6984831b050606f318798486b
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-24 23:54:50 +0530

    Added LinkedListOperations insertAtHead

--- src/main/java/com/gaganbelgur/dsa/linkedlist/LinkedListOperations.java
+++ src/main/java/com/gaganbelgur/dsa/linkedlist/LinkedListOperations.java
@@ -1,4 +1,11 @@
 package main.java.com.gaganbelgur.dsa.linkedlist;
 
 public class LinkedListOperations {
+
+    public ListNode insertAtHead(ListNode head, int X) {
+        ListNode node = new ListNode(X);
+        node.next = head;
+        return node;
+    }
+
 }
