aae81f9f2921dfccc204dbb6a0063f1e64082786
GaganBelgur <gagan.belgur48@gmail.com>
2025-09-27 23:28:00 +0530

    Added DoubleLinkedListOperations and DoubleLinkedListOperationsImpl

--- src/main/java/com/gaganbelgur/dsa/linkedlist/doublelinkedlist/DoubleLinkedListOperationsImpl.java
+++ src/main/java/com/gaganbelgur/dsa/linkedlist/doublelinkedlist/DoubleLinkedListOperationsImpl.java
@@ -1,6 +1,7 @@
 package main.java.com.gaganbelgur.dsa.linkedlist.doublelinkedlist;
 
 public class DoubleLinkedListOperationsImpl implements DoubleLinkedListOperations {
+
     @Override
     public DoubleLinkedListNode insertBeforeHead(DoubleLinkedListNode head, int data) {
         DoubleLinkedListNode temp = new DoubleLinkedListNode(data);
@@ -8,4 +9,5 @@ public class DoubleLinkedListOperationsImpl implements DoubleLinkedListOperation
         head.setPrev(temp);
         return temp;
     }
+
 }
