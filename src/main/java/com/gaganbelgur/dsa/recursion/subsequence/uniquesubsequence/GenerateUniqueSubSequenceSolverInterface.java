24e116d1902cf23f94ff54228848dba23de65bb6
GaganBelgur <gagan.belgur48@gmail.com>
2025-10-06 20:20:14 +0530

    Added GenerateUniqueSubSequenceSolverInterface

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/subsequence/uniquesubsequence/GenerateUniqueSubSequenceSolverInterface.java
@@ -0,0 +1,8 @@
+package main.java.com.gaganbelgur.dsa.recursion.subsequence.uniquesubsequence;
+
+import java.util.List;
+
+public interface GenerateUniqueSubSequenceSolverInterface {
+
+    List<List<Integer>> subsetsWithDup(int[] nums);
+}
