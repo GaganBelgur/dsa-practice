382aaed8ea543fa4d6ac68bbc78ddb2045914947
GaganBelgur <gagan.belgur48@gmail.com>
2025-11-06 23:24:52 +0530

    Added interface for letter combinations solver

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/lettercombinations/LetterCombinationsSolverInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.gaganbelgur.dsa.recursion.lettercombinations;
+
+import java.util.List;
+
+public interface LetterCombinationsSolverInterface {
+
+    List<String> letterCombinations(String digits);
+
+}
