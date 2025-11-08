4d8c008ac9ba39e6fd9041accfbc52cda587dac9
GaganBelgur <gagan.belgur48@gmail.com>
2025-11-08 17:53:52 +0530

    Add interface for palindrome partition solver

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/subsequence/palindromepartition/PalindromePartitionSolverInterface.java
@@ -0,0 +1,7 @@
+package main.java.com.gaganbelgur.dsa.recursion.subsequence.palindromepartition;
+
+import java.util.List;
+
+public interface PalindromePartitionSolverInterface {
+    List<List<String>> partition(String s);
+}
