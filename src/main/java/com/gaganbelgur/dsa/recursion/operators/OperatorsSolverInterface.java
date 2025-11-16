9afe7d8df6232497ff88287b2816d47cc849ad1b
GaganBelgur <gagan.belgur48@gmail.com>
2025-11-16 15:49:10 +0530

    Add OperatorsSolverInterface with addOperators method signature

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/operators/OperatorsSolverInterface.java
@@ -0,0 +1,7 @@
+package main.java.com.gaganbelgur.dsa.recursion.operators;
+
+import java.util.List;
+
+public interface OperatorsSolverInterface {
+    public List<String> addOperators(String num, int target);
+}
