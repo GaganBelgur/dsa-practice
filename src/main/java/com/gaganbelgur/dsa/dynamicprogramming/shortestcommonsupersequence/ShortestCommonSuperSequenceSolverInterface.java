0994be867259a31d6ea5a84a27e7a90a1941a7b8
GaganBelgur <gagan.belgur48@gmail.com>
2025-12-17 19:52:01 +0530

    Add ShortestCommonSuperSequenceSolver interface for shortest common supersequence calculation

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/shortestcommonsupersequence/ShortestCommonSuperSequenceSolverInterface.java
@@ -0,0 +1,6 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.shortestcommonsupersequence;
+
+public interface ShortestCommonSuperSequenceSolverInterface {
+
+    String shortestCommonSupersequence(String str1, String str2);
+}
