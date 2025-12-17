38a8b5295df45bd6329e797ee2d68197ca3c89ec
GaganBelgur <gagan.belgur48@gmail.com>
2025-12-17 19:58:51 +0530

    Add test class for ShortestCommonSuperSequenceSolverTabulation implementation

---
+++ src/test/java/com/gaganbelgur/dsa/dynamicprogramming/shortestcommonsupersequence/ShortestCommonSuperSequenceSolverTabulationTest.java
@@ -0,0 +1,15 @@
+package test.java.com.gaganbelgur.dsa.dynamicprogramming.shortestcommonsupersequence;
+
+import main.java.com.gaganbelgur.dsa.dynamicprogramming.shortestcommonsupersequence.ShortestCommonSuperSequenceSolverInterface;
+import main.java.com.gaganbelgur.dsa.dynamicprogramming.shortestcommonsupersequence.ShortestCommonSuperSequenceSolverTabulation;
+
+public class ShortestCommonSuperSequenceSolverTabulationTest extends BaseShortestCommonSuperSequenceSolverTest {
+
+    private final ShortestCommonSuperSequenceSolverTabulation solver = new ShortestCommonSuperSequenceSolverTabulation();
+
+
+    @Override
+    protected ShortestCommonSuperSequenceSolverInterface getSolver() {
+        return solver;
+    }
+}
