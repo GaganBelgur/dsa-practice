36a84fdbb589b0c5c246566b3cac9cd6269c2bdc
GaganBelgur <gagan.belgur48@gmail.com>
2025-12-17 13:22:35 +0530

    Add LongestPalindromeSubSequenceSolver interface for palindrome subsequence calculation

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/palindrome/longestpalindrome/LongestPalindromeSubSequenceSolver.java
@@ -0,0 +1,6 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.palindrome.longestpalindrome;
+
+public interface LongestPalindromeSubSequenceSolver {
+
+    int longestPalindromeSubsequence(String s);
+}
