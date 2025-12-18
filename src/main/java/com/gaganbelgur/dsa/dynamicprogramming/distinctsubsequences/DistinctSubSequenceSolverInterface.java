bcfb28dabeee6906d2c8d2380aa0539a3ddc092e
GaganBelgur <gagan.belgur48@gmail.com>
2025-12-18 23:25:28 +0530

    Add DistinctSubSequenceSolver interface for distinct subsequence calculation

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/distinctsubsequences/DistinctSubSequenceSolverInterface.java
@@ -0,0 +1,6 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.distinctsubsequences;
+
+public interface DistinctSubSequenceSolverInterface {
+
+    int distinctSubsequences(String s1, String s2);
+}
