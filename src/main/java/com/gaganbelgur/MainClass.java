fc2447a10ee37cce4000c1a006523bd928cacf8a
GaganBelgur <gagan.belgur48@gmail.com>
2026-03-22 14:13:32 +0530

    Updated the Main class to test all the approaches.

--- src/main/java/com/gaganbelgur/MainClass.java
+++ src/main/java/com/gaganbelgur/MainClass.java
@@ -3,6 +3,7 @@ package main.java.com.gaganbelgur;
 import main.java.com.gaganbelgur.dsa.arrays.longestconsecutivesequence.FindLongestConsecutiveSequenceInterface;
 import main.java.com.gaganbelgur.dsa.arrays.longestconsecutivesequence.FindLongestConsecutiveSequenceInterfaceBetterImpl;
 import main.java.com.gaganbelgur.dsa.arrays.longestconsecutivesequence.FindLongestConsecutiveSequenceInterfaceBruteForceImpl;
+import main.java.com.gaganbelgur.dsa.arrays.longestconsecutivesequence.FindLongestConsecutiveSequenceOptimalApproachImpl;
 import main.java.com.gaganbelgur.dsa.sorting.MergeSort;
 import main.java.com.gaganbelgur.dsa.sorting.QuickSort;
 import main.java.com.gaganbelgur.dsa.sorting.SortInterface;
@@ -22,14 +23,14 @@ public class MainClass {
         quickSort.sort(quickSortInputArray);
         System.out.println(Arrays.toString(quickSortInputArray));
 
-        FindLongestConsecutiveSequenceInterface findLongestConsecutiveSequenceInterface = new FindLongestConsecutiveSequenceInterfaceBruteForceImpl();
+        findLongestConsecutiveSequence(new FindLongestConsecutiveSequenceInterfaceBruteForceImpl());
+        findLongestConsecutiveSequence(new FindLongestConsecutiveSequenceInterfaceBetterImpl());
+        findLongestConsecutiveSequence(new FindLongestConsecutiveSequenceOptimalApproachImpl());
+    }
+
+    private static void findLongestConsecutiveSequence(FindLongestConsecutiveSequenceInterface findLongestConsecutiveSequenceInterface) {
         int[] inputArray = {100, 4, 200, 1, 3, 2};
         int result = findLongestConsecutiveSequenceInterface.longestConsecutive(inputArray);
         System.out.println(result);
-
-        FindLongestConsecutiveSequenceInterface betterApproach = new FindLongestConsecutiveSequenceInterfaceBetterImpl();
-        int[] inputArrayForBetterApproach = {100, 4, 200, 1, 3, 2};
-        int betterApproachResult = betterApproach.longestConsecutive(inputArrayForBetterApproach);
-        System.out.println(betterApproachResult);
     }
 }
