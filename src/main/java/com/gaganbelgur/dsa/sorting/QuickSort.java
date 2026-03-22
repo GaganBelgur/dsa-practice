f1d5651acc22beaf1ae0dd4fa67e7952991f2aa6
GaganBelgur <gagan.belgur48@gmail.com>
2026-03-22 12:02:14 +0530

    Added the quick sort algorithm.

--- src/main/java/com/gaganbelgur/dsa/sorting/QuickSort.java
+++ src/main/java/com/gaganbelgur/dsa/sorting/QuickSort.java
@@ -3,6 +3,9 @@ package main.java.com.gaganbelgur.dsa.sorting;
 public class QuickSort implements SortInterface {
     @Override
     public void sort(int[] inputArray) {
+        if (inputArray == null || inputArray.length < 1) {
+            return;
+        }
         quickSortHelper(inputArray, 0, inputArray.length - 1);
     }
 
