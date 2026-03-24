92cc796d52d48ca31cc6140bd43c61cbf5e25ed4
GaganBelgur <gagan.belgur48@gmail.com>
2026-03-24 23:28:10 +0530

    Added the PascalTriangleSolverInterface

---
+++ src/main/java/com/gaganbelgur/dsa/arrays/pascaltriangle/PascalTriangleSolverInterface.java
@@ -0,0 +1,8 @@
+package main.java.com.gaganbelgur.dsa.arrays.pascaltriangle;
+
+import java.util.List;
+
+public interface PascalTriangleSolverInterface {
+
+    List<List<Integer>> pascalTriangle(int n);
+}
