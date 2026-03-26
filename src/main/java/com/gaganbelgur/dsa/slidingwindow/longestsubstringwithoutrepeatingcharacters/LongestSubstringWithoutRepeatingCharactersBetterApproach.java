8dcc6acac2bbb9d88083687a3d914c2c017816dc
GaganBelgur <gagan.belgur48@gmail.com>
2026-03-26 23:59:16 +0530

    Implemented the LongestSubstringWithoutRepeatingCharactersBetterApproach

---
+++ src/main/java/com/gaganbelgur/dsa/slidingwindow/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersBetterApproach.java
@@ -0,0 +1,25 @@
+package main.java.com.gaganbelgur.dsa.slidingwindow.longestsubstringwithoutrepeatingcharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharactersBetterApproach implements LongestSubstringWithoutRepeatingCharactersSolverInterface {
+    @Override
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+
+            map.put(c, right);
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
