cae09e0a110c815d8372383f4fea54779d214d94
GaganBelgur <gagan.belgur48@gmail.com>
2026-03-31 23:56:58 +0530

    Updated the Trie Node with advanced operations

--- src/main/java/com/gaganbelgur/dsa/trie/TrieNode.java
+++ src/main/java/com/gaganbelgur/dsa/trie/TrieNode.java
@@ -6,6 +6,14 @@ public class TrieNode {
 
     private boolean isWordEnd = false;
 
+    private int countEndWith = 0;
+
+    private int prefixCount = 0;
+
+    boolean containsKey(char ch) {
+        return trieNodes[ch - 'a'] != null;
+    }
+
     void put(char ch, TrieNode node) {
         trieNodes[ch - 'a'] = node;
     }
@@ -14,15 +22,35 @@ public class TrieNode {
         return trieNodes[ch - 'a'];
     }
 
-    boolean containsKey(char ch) {
-        return trieNodes[ch - 'a'] != null;
-    }
-
     void markEndOfWord() {
         isWordEnd = true;
     }
 
     boolean getIsWordEnd() {
         return isWordEnd;
+    }
+
+    public void increaseEnd() {
+        countEndWith++;
+    }
+
+    public void increasePrefixCount() {
+        prefixCount++;
+    }
+
+    public int getCountEndWith() {
+        return countEndWith;
+    }
+
+    public int getPrefixCount() {
+        return prefixCount;
+    }
+
+    public void decreasePrefixCount() {
+        prefixCount--;
+    }
+
+    public void decreaseEndCount() {
+        countEndWith--;
     }
 }
