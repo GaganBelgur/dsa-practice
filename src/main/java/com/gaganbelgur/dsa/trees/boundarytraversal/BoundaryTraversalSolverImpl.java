9964f88cd1e6c313f071c1dbf6dea4ce14e2d597
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-07 22:15:55 +0530

    Implemented the BoundaryTraversalSolverImpl

---
+++ src/main/java/com/gaganbelgur/dsa/trees/boundarytraversal/BoundaryTraversalSolverImpl.java
@@ -0,0 +1,63 @@
+package main.java.com.gaganbelgur.dsa.trees.boundarytraversal;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoundaryTraversalSolverImpl implements BoundaryTraversalSolverInterface {
+    @Override
+    public List<Integer> boundary(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        if (!isLeafNode(root)) result.add(root.getData());
+
+        traverseLeftBoundary(root.getLeft(), result);
+        traverseOnlyLeaves(root, result);
+        traverseRightBoundary(root.getRight(), result);
+
+        return result;
+    }
+
+    private void traverseLeftBoundary(TreeNode node, List<Integer> result) {
+        while (node != null) {
+            if (!isLeafNode(node))  result.add(node.getData());
+
+            if (node.getLeft() != null) node = node.getLeft();
+            else node = node.getRight();
+        }
+    }
+
+    private void traverseRightBoundary(TreeNode node, List<Integer> result) {
+        List<Integer> temp = new ArrayList<>();
+
+        while (node != null) {
+            if (!isLeafNode(node)) temp.add(node.getData());
+
+            if (node.getRight() != null) node = node.getRight();
+            else node = node.getLeft();
+        }
+
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            result.add(temp.get(i));
+        }
+    }
+
+    private void traverseOnlyLeaves(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        if (isLeafNode(node)) {
+            result.add(node.getData());
+            return;
+        }
+
+        traverseOnlyLeaves(node.getLeft(), result);
+        traverseOnlyLeaves(node.getRight(), result);
+    }
+
+    private boolean isLeafNode(TreeNode node) {
+        return node.getLeft() == null && node.getRight() == null;
+    }
+}
---
+++ src/main/java/com/gaganbelgur/dsa/trees/boundarytraversal/BoundaryTraversalSolverInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.gaganbelgur.dsa.trees.boundarytraversal;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.List;
+
+public interface BoundaryTraversalSolverInterface {
+    List<Integer> boundary(TreeNode root);
+}
