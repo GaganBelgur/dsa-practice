8084273439ba19c4d5ce3772a28f1a353749daed
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-07 22:30:48 +0530

    Added the InOrderTraversalSolverInterface

---
+++ src/main/java/com/gaganbelgur/dsa/trees/traversal/inorder/InOrderTraversalSolverInterface.java
@@ -0,0 +1,10 @@
+package main.java.com.gaganbelgur.dsa.trees.traversal.inorder;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.List;
+
+public interface InOrderTraversalSolverInterface {
+
+    List<Integer> inorder(TreeNode root);
+}
