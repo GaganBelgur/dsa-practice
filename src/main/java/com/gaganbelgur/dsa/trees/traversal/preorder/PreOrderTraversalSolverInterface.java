4dcf3043d317ed7adc7b34d9c835a1543275b450
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-07 22:26:34 +0530

    Added the PreOrderTraversalSolverInterface

---
+++ src/main/java/com/gaganbelgur/dsa/trees/traversal/preorder/PreOrderTraversalSolverInterface.java
@@ -0,0 +1,10 @@
+package main.java.com.gaganbelgur.dsa.trees.traversal.preorder;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.List;
+
+public interface PreOrderTraversalSolverInterface {
+
+    List<Integer> preorderTraversal(TreeNode node);
+}
