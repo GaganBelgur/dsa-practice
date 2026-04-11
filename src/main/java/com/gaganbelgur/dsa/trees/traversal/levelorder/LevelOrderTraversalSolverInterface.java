ef354b15865768c11257ded282f937597947d35f
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-11 21:57:04 +0530

    Added the level order traversal interface.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/traversal/levelorder/LevelOrderTraversalSolverInterface.java
@@ -0,0 +1,10 @@
+package main.java.com.gaganbelgur.dsa.trees.traversal.levelorder;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.List;
+
+public interface LevelOrderTraversalSolverInterface {
+
+    List<Integer> levelOrder(TreeNode root);
+}
