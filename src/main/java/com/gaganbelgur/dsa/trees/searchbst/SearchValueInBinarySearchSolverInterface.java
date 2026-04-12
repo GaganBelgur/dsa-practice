d90e34c9dfd6a7a4bd02ed1344645f919f94fc10
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-12 23:36:31 +0530

    Added the Interface to find the value in binary search tree.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/searchbst/SearchValueInBinarySearchSolverInterface.java
@@ -0,0 +1,8 @@
+package main.java.com.gaganbelgur.dsa.trees.searchbst;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+public interface SearchValueInBinarySearchSolverInterface {
+
+    TreeNode searchBST(TreeNode root, int val);
+}
