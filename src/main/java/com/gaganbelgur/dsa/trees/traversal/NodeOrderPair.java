18ace3c1790833eeae8c98b6d61d43bdaaf18d15
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-14 23:03:08 +0530

    Added the NodeOrderPair class.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/traversal/NodeOrderPair.java
@@ -0,0 +1,13 @@
+package main.java.com.gaganbelgur.dsa.trees.traversal;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+class NodeOrderPair {
+    TreeNode node;
+    int order;
+
+    public NodeOrderPair(TreeNode node, int order) {
+        this.node = node;
+        this.order = order;
+    }
+}
