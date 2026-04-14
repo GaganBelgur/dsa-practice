d6dec815304848b1243ea8e291a2690f5a4a7fbc
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-14 23:05:46 +0530

    Added the interface to have one iteration to get the result of all 3 traversals [In, Pre and Post].

---
+++ src/main/java/com/gaganbelgur/dsa/trees/traversal/AllTraversalInOneIterationSolverInterface.java
@@ -0,0 +1,10 @@
+package main.java.com.gaganbelgur.dsa.trees.traversal;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.List;
+
+public interface AllTraversalInOneIterationSolverInterface {
+
+    List<List<Integer>> treeTraversal(TreeNode root);
+}
