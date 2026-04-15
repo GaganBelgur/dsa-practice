9d0ce77f22591b1e1384ddb369bebe6df7dcf0c9
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-15 23:33:48 +0530

    Added the implementation for Top View for Binary Trees.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/views/BinaryTreesTopViewSolverImpl.java
@@ -0,0 +1,31 @@
+package main.java.com.gaganbelgur.dsa.trees.views;
+
+import main.java.com.gaganbelgur.dsa.trees.NodeOrderPair;
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.*;
+
+public class BinaryTreesTopViewSolverImpl implements BinaryTreesTopViewSolverInterface {
+    @Override
+    public List<Integer> topView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<NodeOrderPair> queue = new LinkedList<>();
+        queue.add(new NodeOrderPair(root, 0));
+
+        while(!queue.isEmpty()) {
+            NodeOrderPair pair = queue.poll();
+            TreeNode node = pair.node;
+            int order = pair.order;
+
+            if(!map.containsKey(order)) map.put(order, node.getData());
+
+            if(node.getLeft() != null) queue.add(new NodeOrderPair(node.getLeft(), order - 1));
+            if(node.getRight() != null) queue.add(new NodeOrderPair(node.getRight(), order + 1));
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
