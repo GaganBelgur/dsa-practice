be30272d2a13be1195141d1268689f5efb08eb33
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-16 21:20:10 +0530

    Added the bottom view implementation for binary trees.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/views/BottomViewSolverImpl.java
@@ -0,0 +1,31 @@
+package main.java.com.gaganbelgur.dsa.trees.views;
+
+import main.java.com.gaganbelgur.dsa.trees.NodeOrderPair;
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+import java.util.*;
+
+public class BottomViewSolverImpl implements BinaryTreesViewSolverInterface {
+    @Override
+    public List<Integer> view(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<NodeOrderPair> queue = new LinkedList<>();
+        queue.add(new NodeOrderPair(root, 0));
+
+        while(!queue.isEmpty()) {
+            NodeOrderPair pair = queue.poll();
+            TreeNode node = pair.node;
+            int order = pair.order;
+
+            map.put(order, node.getData());
+
+            if(node.getLeft() != null) queue.add(new NodeOrderPair(node.getLeft(), order - 1));
+            if(node.getRight() != null) queue.add(new NodeOrderPair(node.getRight(), order + 1));
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
