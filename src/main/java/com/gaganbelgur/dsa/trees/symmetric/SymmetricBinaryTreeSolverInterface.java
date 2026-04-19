0e61ff4b3981ce1a8d9796feb7ac65608aad714d
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-19 20:30:27 +0530

    Added the interface to find the symmetric binary tree.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/symmetric/SymmetricBinaryTreeSolverInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.gaganbelgur.dsa.trees.symmetric;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+public interface SymmetricBinaryTreeSolverInterface {
+
+    boolean isSymmetric(TreeNode root);
+
+}
