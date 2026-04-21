727e6159102ec7c78eb67a1fa890fc59dd2a00ce
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-21 23:07:59 +0530

    Added the interface to find the nodes present in binary tree.

---
+++ src/main/java/com/gaganbelgur/dsa/trees/countnodes/CountNodesSolverInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.gaganbelgur.dsa.trees.countnodes;
+
+import main.java.com.gaganbelgur.dsa.trees.TreeNode;
+
+public interface CountNodesSolverInterface {
+
+    int countNodes(TreeNode root);
+
+}
