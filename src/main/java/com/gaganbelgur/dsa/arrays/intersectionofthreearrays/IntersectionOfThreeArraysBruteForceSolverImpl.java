9bbd64e15153b3a5cc022c41d1978826caed6353
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-22 23:26:14 +0530

    Added the brute force approach to find the intersection of three arrays.

--- src/main/java/com/gaganbelgur/dsa/arrays/intersectionofthreearrays/IntersectionOfThreeArraysBruteForceSolverImpl.java
+++ src/main/java/com/gaganbelgur/dsa/arrays/intersectionofthreearrays/IntersectionOfThreeArraysBruteForceSolverImpl.java
@@ -6,9 +6,6 @@ import java.util.List;
 public class IntersectionOfThreeArraysBruteForceSolverImpl implements IntersectionOfThreeArraysSolverInterface {
     @Override
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
-        int a1 = arr1.length;
-        int a2 = arr2.length;
-        int a3 = arr3.length;
         List<Integer> result = new ArrayList<>();
 
         for (int element : arr1) {
