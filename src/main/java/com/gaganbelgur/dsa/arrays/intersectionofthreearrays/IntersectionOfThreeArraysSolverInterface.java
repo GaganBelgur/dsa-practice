b19d0de4185b9f372142371215422eb3ea5bca65
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-22 23:24:44 +0530

    Added the interface to find the intersection of three arrays.

---
+++ src/main/java/com/gaganbelgur/dsa/arrays/intersectionofthreearrays/IntersectionOfThreeArraysSolverInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.gaganbelgur.dsa.arrays.intersectionofthreearrays;
+
+import java.util.List;
+
+public interface IntersectionOfThreeArraysSolverInterface {
+
+    List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3);
+
+}
