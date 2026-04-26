e6db12e06f9f1f78c78b08c0b2a33cc49ecf3618
GaganBelgur <gagan.belgur48@gmail.com>
2026-04-26 22:54:19 +0530

    Added the implementation to find the fixed point.

--- src/main/java/com/gaganbelgur/dsa/binarysearch/fixedpoint/FixedPointSolverInterface.java
+++ src/main/java/com/gaganbelgur/dsa/binarysearch/fixedpoint/FixedPointSolverInterface.java
@@ -1,5 +1,15 @@
 package main.java.com.gaganbelgur.dsa.binarysearch.fixedpoint;
 
+/*
+Given an array of distinct integers arr, where arr is sorted in ascending order, return the smallest index i that satisfies arr[i] == i.
+If there is no such index, return -1.
+
+Example 1
+Input: arr = [-8, -3, 2, 5, 9]
+Output: 2
+Explanation: arr[2] = 2, thus the output is 2.
+*/
+
 public interface FixedPointSolverInterface {
 
     int fixedPoint(int[] arr);
