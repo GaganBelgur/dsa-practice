06f7c16a6d171c5cf6552bcdf3a6c42df51bf6a0
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-03 23:42:19 +0530

    Updated the main test class.

--- src/main/java/com/gaganbelgur/implementation/hitcounter/MainTest.java
+++ src/main/java/com/gaganbelgur/implementation/hitcounter/MainTest.java
@@ -2,11 +2,17 @@ package main.java.com.gaganbelgur.implementation.hitcounter;
 
 public class MainTest {
     public static void main(String[] args) {
-        HitCounterSolverInterface counterSolverInterface = new HitCounterBruteForceSolverImpl();
+        verify(new HitCounterBruteForceSolverImpl());
+        verify(new HitCounterBetterApproachSolverImpl());
+    }
+
+    private static void verify(HitCounterSolverInterface counterSolverInterface) {
         counterSolverInterface.hit(1);
         counterSolverInterface.hit(2);
         counterSolverInterface.hit(3);
+        counterSolverInterface.hit(4);
+        int result = counterSolverInterface.getHits(2);
 
-        System.out.println(counterSolverInterface.getHits(3));
+        System.out.println(result);
     }
 }
