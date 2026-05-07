3e4f81cc2141ef58608cb00c2240a894f3157918
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-07 23:26:49 +0530

    Added the problem statement.

--- src/main/java/com/gaganbelgur/dsa/dynamicprogramming/countsubsetsum/CountSubsetSumSolverInterface.java
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/countsubsetsum/CountSubsetSumSolverInterface.java
@@ -1,5 +1,16 @@
 package main.java.com.gaganbelgur.dsa.dynamicprogramming.countsubsetsum;
 
+/*Given an array arr of n integers and an integer K,
+count the number of subsets of the given array that have a sum equal to K.
+Return the result modulo (109 + 7).
+
+Example 1
+
+Input: arr = [2, 3, 5, 16, 8, 10], K = 10
+Output: 3
+
+Explanation: The subsets are [2, 8], [10], and [2, 3, 5].
+*/
 public interface CountSubsetSumSolverInterface {
 
     int perfectSum(int[] arr, int K);
