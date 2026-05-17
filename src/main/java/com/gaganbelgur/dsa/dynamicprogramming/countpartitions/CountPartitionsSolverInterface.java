44be70aa6d2ca99c030d17f9b2d65d7d5fbe2cc2
GaganBelgur <gagan.belgur48@gmail.com>
2026-05-17 20:12:33 +0530

    Added the CountPartitionsSolverInterface to count the number of ways to partition the array into 2 subsets.

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/countpartitions/CountPartitionsSolverInterface.java
@@ -0,0 +1,5 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.countpartitions;
+
+public interface CountPartitionsSolverInterface {
+    int countPartitions(int n, int diff, int[] arr);
+}
