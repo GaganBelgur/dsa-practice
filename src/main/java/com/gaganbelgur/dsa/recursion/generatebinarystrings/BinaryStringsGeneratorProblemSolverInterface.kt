bb7d6f863a3f32568c5a361a5cc202f18c4d8c1d
GaganBelgur <gagan.belgur48@gmail.com>
2026-07-28 23:50:46 +0530

    Add BinaryStringsGeneratorProblemSolverInterface for generating binary strings

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/generatebinarystrings/BinaryStringsGeneratorProblemSolverInterface.kt
@@ -0,0 +1,5 @@
+package main.java.com.gaganbelgur.dsa.recursion.generatebinarystrings
+
+fun interface BinaryStringsGeneratorProblemSolverInterface {
+    fun generateBinaryStrings(n : Int): List<String>
+}
