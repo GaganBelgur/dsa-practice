d0c37ed8cd7771b94b43b9aebe04f17718615fe2
GaganBelgur <gagan.belgur48@gmail.com>
2026-07-28 23:53:14 +0530

    Add unit tests for BinaryStringsSolverImpl to validate binary string generation

---
+++ src/test/java/com/gaganbelgur/dsa/recursion/BinaryStringGeneratorSolverTest.kt
@@ -0,0 +1,20 @@
+package test.java.com.gaganbelgur.dsa.recursion
+
+import main.java.com.gaganbelgur.dsa.recursion.generatebinarystrings.BinaryStringsGeneratorProblemSolverInterface
+import main.java.com.gaganbelgur.dsa.recursion.generatebinarystrings.BinaryStringsSolverImpl
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+@Suppress("FunctionName")
+class BinaryStringGeneratorSolverTest {
+
+    val binaryStringsSolverImpl: BinaryStringsGeneratorProblemSolverInterface = BinaryStringsSolverImpl()
+
+    @Test
+    fun `Given positive integer When generate binary strings Then returned list should be of positive size`() {
+       val input = 3
+       val expectedList = listOf("000", "001", "010", "100", "101")
+       val result = binaryStringsSolverImpl.generateBinaryStrings(input)
+       assertEquals(expectedList, result)
+    }
+}
