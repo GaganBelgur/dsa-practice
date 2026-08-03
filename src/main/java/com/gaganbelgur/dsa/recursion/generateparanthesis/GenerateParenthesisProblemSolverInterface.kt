1333cd5db78d857a2313551e7054b43749bb06e9
GaganBelgur <gagan.belgur48@gmail.com>
2026-08-03 23:18:12 +0530

    Add GenerateParenthesisProblemSolverInterface for generating valid parentheses combinations

---
+++ src/main/java/com/gaganbelgur/dsa/recursion/generateparanthesis/GenerateParenthesisProblemSolverInterface.kt
@@ -0,0 +1,6 @@
+package main.java.com.gaganbelgur.dsa.recursion.generateparanthesis
+
+fun interface GenerateParenthesisProblemSolverInterface {
+
+    fun generateParenthesis(n: Int): List<String>
+}
