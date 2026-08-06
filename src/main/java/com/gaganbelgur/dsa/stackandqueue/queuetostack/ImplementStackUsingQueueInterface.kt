4ef957d2b15a770ca000570c6b07734e10138f6f
GaganBelgur <gagan.belgur48@gmail.com>
2026-08-06 23:11:08 +0530

    Add ImplementStackUsingQueueInterface for stack operations using queues

---
+++ src/main/java/com/gaganbelgur/dsa/stackandqueue/queuetostack/ImplementStackUsingQueueInterface.kt
@@ -0,0 +1,8 @@
+package main.java.com.gaganbelgur.dsa.stackandqueue.queuetostack
+
+interface ImplementStackUsingQueueInterface {
+    fun push(num: Int)
+    fun pop(): Int
+    fun isEmpty(): Boolean
+    fun top(): Int
+}
