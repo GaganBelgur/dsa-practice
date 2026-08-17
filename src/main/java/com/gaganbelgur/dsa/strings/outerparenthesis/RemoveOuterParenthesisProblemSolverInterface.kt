33323212a60866c4c19f6aa81a53ee915a41249b
GaganBelgur <gagan.belgur48@gmail.com>
2026-08-17 23:37:44 +0530

    Add interface for Remove Outer Parenthesis Problem Solver.

---
+++ src/main/java/com/gaganbelgur/dsa/strings/outerparenthesis/RemoveOuterParenthesisProblemSolverInterface.kt
@@ -0,0 +1,5 @@
+package main.java.com.gaganbelgur.dsa.strings.outerparenthesis
+
+fun interface RemoveOuterParenthesisProblemSolverInterface {
+    fun removeOuterParentheses(input: String): String
+}
