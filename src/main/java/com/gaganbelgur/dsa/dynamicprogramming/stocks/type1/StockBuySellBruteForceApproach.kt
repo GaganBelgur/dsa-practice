11d71e9b50201ac99a6a78d7dee7e425502d1bd1
GaganBelgur <gagan.belgur48@gmail.com>
2026-09-14 23:40:18 +0530

    Add brute force approach for stock buy and sell problem

---
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/stocks/type1/StockBuySellBruteForceApproach.kt
@@ -0,0 +1,18 @@
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.stocks.type1
+
+import main.java.com.gaganbelgur.dsa.dynamicprogramming.stocks.StocksBuySellSolverInterface
+
+class StockBuySellBruteForceApproach: StocksBuySellSolverInterface {
+    override fun stockBuySell(arr: IntArray?, n: Int): Int {
+        var maxProfit = 0
+        for (i in arr!!.indices) {
+            for (j in i + 1 until arr.size) {
+                val profit = arr[j] - arr[i]
+                if (profit > maxProfit) {
+                    maxProfit = profit
+                }
+            }
+        }
+        return maxProfit
+    }
+}
