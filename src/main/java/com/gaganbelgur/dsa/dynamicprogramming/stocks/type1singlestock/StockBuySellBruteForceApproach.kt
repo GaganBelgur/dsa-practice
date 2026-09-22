d569ff670e6ecb78e8aa87450c749456d7806b44
GaganBelgur <gagan.belgur48@gmail.com>
2026-09-22 06:44:20 +0530

    Rename package for stock buy and sell approaches to reflect single stock context

--- src/main/java/com/gaganbelgur/dsa/dynamicprogramming/stocks/type1singlestock/StockBuySellBruteForceApproach.kt
+++ src/main/java/com/gaganbelgur/dsa/dynamicprogramming/stocks/type1singlestock/StockBuySellBruteForceApproach.kt
@@ -1,4 +1,4 @@
-package main.java.com.gaganbelgur.dsa.dynamicprogramming.stocks.type1
+package main.java.com.gaganbelgur.dsa.dynamicprogramming.stocks.type1singlestock
 
 import main.java.com.gaganbelgur.dsa.dynamicprogramming.stocks.StocksBuySellSolverInterface
 
